f5587700be79055c973ab73c5f0ac05cb2cfad8c
KangJaeHoon <morateng@naver.com>
2024-06-23 15:59:07 +0900

    solve 카드뭉치

--- 7장/카드뭉치.py
+++ 7장/카드뭉치.py
@@ -3,16 +3,9 @@ from collections import deque
 def solution(cards1, cards2, goal):
     
     result = 'Yes'
-    card1 = deque()
-    card2 = deque()
-    goal_que = deque()
-    
-    for i in cards1:
-        card1.append(i)
-    for i in cards2:
-        card2.append(i)
-    for i in goal:
-        goal_que.append(i)
+    card1 = deque(cards1)
+    card2 = deque(cards2)
+    goal_que = deque(goal)
     
     while(goal_que):
         if goal_que[0] == card1[0]:
